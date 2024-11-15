0890c34ecac0eaefcac3891bf70c38d7b2ec9059
Giang Vu <dhgiang.vu@gmail.com>
2024-11-15 19:11:08 +1100

    feat: sign up successful card

--- fs-client/src/pages/SignUp.jsx
+++ fs-client/src/pages/SignUp.jsx
@@ -9,12 +9,12 @@ import LoadingButton from "../components/LoadingButton";
 function SignupPage() {
 
   const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
 
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,69 +22,83 @@ function SignupPage() {
     setLoading(true);
 
     createNewAccount(username, email, password)
-      .then(() => navigate("/login"))
+      .then(() => setSuccess(true))
       .catch((error) => alert(error))
       .finally(() => setLoading(false));
   }
 
+  if (success) {
+    return (
+      <div className="auth-container">
+        <div className="success-card">
+          <h2>Account Registered!</h2>
+          <p>Your account has been created.</p>
+          <Link to="/login" className="login-link">Go to Login</Link>
+        </div>
+      </div>
+    )
+  }
+  else {
 
-  return (
-    <div className="auth-container">
-      <form className="auth-form" onSubmit={handleSubmit}>
-        <h2 className="auth-title">Create an Account</h2>
-        <FormInput 
-          type="text" 
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)} 
-        />
-        <FormInput 
-          type="email" 
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}  
-        />
-        <FormInput 
-          type="password" 
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}  
-        />
-        <FormInput 
-          type="password" 
-          placeholder="Confirm Password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}  
-        />
-        {/* <button type="submit" className="auth-button" onSubmit={() => {}}>Sign Up</button> */}
-        <LoadingButton
-          type="submit"
-          loading={loading}
-          additionalStyles={{
-            padding: "8px",
-            backgroundColor: "var(--button-color)",
-            color: "var(--button-text-color)",
-            border: "none",
-            borderRadius: "var(--border-radius)",
-            fontSize: "16px",
-            width: "100%",
-            cursor: "pointer",
-            transition: "background-color 0.3s",
-            textTransform: "none",
-            "&:hover": {
-              backgroundColor: "#357abd",
-            },
-          }}
-          onSubmit={() => {}}
-        >
-          Sign Up
-        </LoadingButton>
-        <p className="auth-footer">
-          Already have an account? <Link to="../login">Login here</Link>.
-        </p>
-      </form>
-    </div>
-  );
+    return (
+      <div className="auth-container">
+        <form className="auth-form" onSubmit={handleSubmit}>
+          <h2 className="auth-title">Create an Account</h2>
+          <FormInput
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <FormInput
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <FormInput
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FormInput
+            type="password"
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          {/* <button type="submit" className="auth-button" onSubmit={() => {}}>Sign Up</button> */}
+          <LoadingButton
+            type="submit"
+            loading={loading}
+            additionalStyles={{
+              padding: "8px",
+              backgroundColor: "var(--button-color)",
+              color: "var(--button-text-color)",
+              border: "none",
+              borderRadius: "var(--border-radius)",
+              fontSize: "16px",
+              width: "100%",
+              cursor: "pointer",
+              transition: "background-color 0.3s",
+              textTransform: "none",
+              "&:hover": {
+                backgroundColor: "#357abd",
+              },
+            }}
+            onSubmit={() => { }}
+          >
+            Sign Up
+          </LoadingButton>
+          <p className="auth-footer">
+            Already have an account? <Link to="../login">Login here</Link>.
+          </p>
+        </form>
+      </div>
+    );
+  }
+
 }
 
 export default SignupPage;
